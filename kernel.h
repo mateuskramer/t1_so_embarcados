8c89b7ae35190fd56ef2a2ec178f4f7dc76ce2f5
Prof. Anderson Luiz Fernandes Perez <38473840+anderson-perez@users.noreply.github.com>
2026-03-30 14:09:55 -0300

    Versão de 25/03.

--- kernel.h
+++ kernel.h
@@ -17,76 +17,78 @@ void os_start();
 
 TASK idle();
 
-#define SAVE_CONTEXT(state) { \
+#define SAVE_CONTEXT(state) \
 do { \
     if (r_queue.task_running->task_state == RUNNING) { \
-        r_queue.task_running->task_state    = state; \
-        r_queue.task_running->BSR_REG       = BSR; \
-        r_queue.task_running->FSR0H_REG     = FSR0H; \
-        r_queue.task_running->FSR0L_REG     = FSR0L; \
-        r_queue.task_running->FSR1H_REG     = FSR1H; \
-        r_queue.task_running->FSR1L_REG     = FSR1L; \
-        r_queue.task_running->FSR2H_REG     = FSR2H; \
-        r_queue.task_running->FSR2L_REG     = FSR2L; \
-        r_queue.task_running->PCLATH_REG    = PCLATH; \
-        r_queue.task_running->PCLATU_REG    = PCLATU; \
-        r_queue.task_running->PRODH_REG     = PRODH; \
-        r_queue.task_running->PRODL_REG     = PRODL; \
-        r_queue.task_running->STATUS_REG    = STATUS; \
-        r_queue.task_running->TABLAT_REG    = TABLAT; \
-        r_queue.task_running->TBLPTRH_REG   = TBLPTRH; \
-        r_queue.task_running->TBLPTRL_REG   = TBLPTRL; \
-        r_queue.task_running->TBLPTRU_REG   = TBLPTRU; \
-        r_queue.task_running->W_REG         = WREG; \
-        uint8_t size = 0; \
-        do { \
-            r_queue.task_running->task_stack.stack[size].TOSL_REG       = TOSL; \
-            r_queue.task_running->task_stack.stack[size].TOSH_REG       = TOSH; \
-            r_queue.task_running->task_stack.stack[size++].TOSU_REG     = TOSU; \
+        r_queue.task_running->task_state            = state; \
+        r_queue.task_running->BSR_REG               = BSR; \
+        r_queue.task_running->FSR0H_REG             = FSR0H; \
+        r_queue.task_running->FSR0L_REG             = FSR0L; \
+        r_queue.task_running->FSR1H_REG             = FSR1H; \
+        r_queue.task_running->FSR1L_REG             = FSR1L; \
+        r_queue.task_running->FSR2H_REG             = FSR2H; \
+        r_queue.task_running->FSR2L_REG             = FSR2L; \
+        r_queue.task_running->PCLATH_REG            = PCLATH; \
+        r_queue.task_running->PCLATU_REG            = PCLATU; \
+        r_queue.task_running->PRODH_REG             = PRODH; \
+        r_queue.task_running->PRODL_REG             = PRODL; \
+        r_queue.task_running->TABLAT_REG            = TABLAT; \
+        r_queue.task_running->TBLPTRH_REG           = TBLPTRH; \
+        r_queue.task_running->TBLPTRL_REG           = TBLPTRL; \
+        r_queue.task_running->TBLPTRU_REG           = TBLPTRU; \
+        r_queue.task_running->W_REG                 = WREG; \
+        r_queue.task_running->STATUS_REG            = STATUS; \
+        r_queue.task_running->task_stack.stack_size = 0; \
+        while (STKPTR) { \
+            r_queue.task_running->task_stack.stack[r_queue.task_running->task_stack.stack_size].TOSL_REG       = TOSL; \
+            r_queue.task_running->task_stack.stack[r_queue.task_running->task_stack.stack_size].TOSH_REG       = TOSH; \
+            r_queue.task_running->task_stack.stack[r_queue.task_running->task_stack.stack_size].TOSU_REG     = TOSU; \
+            r_queue.task_running->task_stack.stack_size += 1; \
             asm("POP"); \
-        } while (STKPTR); \
-        r_queue.task_running->task_stack.stack_size = size; \
+        } \
     } \
-while (0); \
+} while (0); \
 
-#define RESTORE_CONTEXT() { \
+
+#define RESTORE_CONTEXT() \
 do { \
     if (r_queue.task_running->task_state == READY) { \
-        r_queue.task_running->task_state = RUNNING; \
-        BSR     = r_queue.task_running->BSR_REG; \
-        FSR0H   = r_queue.task_running->FSR0H_REG; \
-        FSR0L   = r_queue.task_running->FSR0L_REG; \
-        FSR1H   = r_queue.task_running->FSR1H_REG; \
-        FSR1L   = r_queue.task_running->FSR1L_REG; \
-        FSR2H   = r_queue.task_running->FSR2H_REG; \
-        FSR2L   = r_queue.task_running->FSR2L_REG; \
-        PCLATH  = r_queue.task_running->PCLATH_REG; \
-        PCLATU  = r_queue.task_running->PCLATU_REG; \
-        PRODH   = r_queue.task_running->PRODH_REG; \
-        PRODL   = r_queue.task_running->PRODL_REG; \
-        STATUS  = r_queue.task_running->STATUS_REG; \
-        TABLAT  = r_queue.task_running->TABLAT_REG; \
-        TBLPTRH = r_queue.task_running->TBLPTRH_REG; \
-        TBLPTRL = r_queue.task_running->TBLPTRL_REG; \
-        TBLPTRU = r_queue.task_running->TBLPTRU_REG; \
-        WREG    = r_queue.task_running->W_REG; \
-        STKPTR  = 0; \
+        r_queue.task_running->task_state    = RUNNING; \
+        BSR                                 = r_queue.task_running->BSR_REG; \
+        FSR0H                               = r_queue.task_running->FSR0H_REG; \
+        FSR0L                               = r_queue.task_running->FSR0L_REG; \
+        FSR1H                               = r_queue.task_running->FSR1H_REG; \
+        FSR1L                               = r_queue.task_running->FSR1L_REG; \
+        FSR2H                               = r_queue.task_running->FSR2H_REG; \
+        FSR2L                               = r_queue.task_running->FSR2L_REG; \
+        PCLATH                              = r_queue.task_running->PCLATH_REG; \
+        PCLATU                              = r_queue.task_running->PCLATU_REG; \
+        PRODH                               = r_queue.task_running->PRODH_REG; \
+        PRODL                               = r_queue.task_running->PRODL_REG; \
+        TABLAT                              = r_queue.task_running->TABLAT_REG; \
+        TBLPTRH                             = r_queue.task_running->TBLPTRH_REG; \
+        TBLPTRL                             = r_queue.task_running->TBLPTRL_REG; \
+        TBLPTRU                             = r_queue.task_running->TBLPTRU_REG; \
+        STATUS                              = r_queue.task_running->STATUS_REG; \
+        WREG                                = r_queue.task_running->W_REG; \
+        STKPTR                              = 0; \
         if (r_queue.task_running->task_stack.stack_size > 0) { \
-            uint8_t size = r_queue.task_running->task_stack.stack_size - 1; \
             do { \
                 asm("PUSH"); \
-                TOSL = r_queue.task_running->task_stack.stack[size].TOSL_REG; \
-                TOSH = r_queue.task_running->task_stack.stack[size].TOSH_REG; \
-                TOSU = r_queue.task_running->task_stack.stack[size--].TOSU_REG; \
-            } while (size); \
+                r_queue.task_running->task_stack.stack_size -= 1; \
+                TOSL = r_queue.task_running->task_stack.stack[r_queue.task_running->task_stack.stack_size].TOSL_REG; \
+                TOSH = r_queue.task_running->task_stack.stack[r_queue.task_running->task_stack.stack_size].TOSH_REG; \
+                TOSU = r_queue.task_running->task_stack.stack[r_queue.task_running->task_stack.stack_size].TOSU_REG; \
+            } while (r_queue.task_running->task_stack.stack_size); \
         } \
         else { \
+            asm("PUSH"); \
             TOSL = (uint8_t)(r_queue.task_running->task_ptr & 0xFF); \
             TOSH = (uint8_t)((r_queue.task_running->task_ptr >> 8) & 0xFF); \
             TOSU = (uint8_t)((r_queue.task_running->task_ptr >> 16) & 0xFF); \
         } \
     } \
-while (0); \
+} while (0); \
 
 
 #endif	/* KERNEL_H */
